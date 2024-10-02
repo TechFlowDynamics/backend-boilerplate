5a384c50fe7190b44dd0e864e0c495d98d442d25
mohitgaur310 <mohitgaur310@gmail.com>
2024-10-02 22:52:37 +0530

    setup main routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import config from "./config";
 
+import routes from "./app/routes/index.routes";
 
 const app = express();
 
@@ -13,6 +14,6 @@ app.use(
   }),
 );
 
-// app.use("/api/v1", routes);
+app.use("/api/v1", routes);
 
 export default app;
