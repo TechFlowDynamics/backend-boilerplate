b9917695ddd9869bfadf7b04d2c2acbd9c069979
mohitgaur310 <mohitgaur310@gmail.com>
2024-10-15 10:23:16 +0530

    feat(validator): validators for step two

--- src/data/validationSchema/userSchema.validator.ts
+++ src/data/validationSchema/userSchema.validator.ts
@@ -10,3 +10,11 @@ export const userSchema = Joi.object({
     .regex(/^[\w](?!.*?\.{2})[\w.]{1,28}[\w]$/, "string"),
   password: Joi.string().min(8).trim().required(),
 });
+
+export const userStepTwoSchema = Joi.object({
+  firstName: Joi.string().min(3).required(),
+  lastName: Joi.string().min(2),
+  phoneNumber: Joi.string().trim().required(),
+  mimeType: Joi.string().required(),
+  fileName: Joi.string().required(),
+});
