525b9c8b54f2eaf6399b6d4ec52b3ce81cfc244a
vivekumar08 <vivekumar2003bsr@gmail.com>
2024-11-01 14:21:21 +0530

    fix(error): errorhanlder in router

--- src/app/routes/index.routes.ts
+++ src/app/routes/index.routes.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from "express";
 import authRouter from "./auth.routes";
 import { ResponseMessages } from "../../core/constants/cloud.constants";
 import { NotFound } from "../middleware/errors";
+import { errorHandler } from "../middleware/errors/errorHandler.middleware";
 const router = express.Router();
 
 router.use("/auth", authRouter);
@@ -12,15 +13,17 @@ router.get(
   "/hello-world",
   async (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json({ data: "Hello from backend" });
-  },
+  }
 );
 
 router.use((req: Request, res: Response, next: NextFunction) => {
   next(
     new NotFound(
-      `${ResponseMessages.RES_MSG_NOT_FOUND_URL_EN}: ${req.originalUrl}`,
-    ),
+      `${ResponseMessages.RES_MSG_NOT_FOUND_URL_EN}: ${req.originalUrl}`
+    )
   );
 });
+
+router.use(errorHandler);
 
 export default router;
