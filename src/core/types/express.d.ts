7a19a3f42874fc5e784b4e42026f44d6e241f2e7
vivekumar08 <vivekumar2003bsr@gmail.com>
2024-11-01 08:58:29 +0530

    feat(express): global attribute in express

---
+++ src/core/types/express.d.ts
@@ -0,0 +1,10 @@
+// src/core/types/express.d.ts
+import "express";
+import { UserType } from "../interface/global/global.interface";
+
+declare module "express" {
+  export interface Request {
+    value?: any;
+    userData?: UserType;
+  }
+}
