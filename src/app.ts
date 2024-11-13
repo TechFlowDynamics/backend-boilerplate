f1bed5a71128bb8cb8af92c9575a394f69fbe59b
vivekumar08 <vivekumar2003bsr@gmail.com>
2024-11-13 22:52:27 +0530

    fix(cors): added public listing

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import express from "express";
 import cors from "cors";
-import config from "./config";
-import { connectToReqDatabase } from "./core/scripts/db.connection";
 
 import routes from "./app/routes/index.routes";
 import { setupLogger } from "./app/middleware/logger.middleware";
-import { errorHandler } from "./app/middleware/errors/errorHandler.middleware";
 
 const app = express();
 setupLogger(app);
@@ -14,14 +11,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
   cors({
-    origin: config.CLIENT_URL,
+    origin: "*",
   }),
 );
 
-// app.use(connectToReqDatabase);
-
 app.use("/api/v1", routes);
-
-// app.use(errorHandler);
 
 export default app;
